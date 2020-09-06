bdfd8bd81d4b7a6e310ab8aa26641b71ce401bb2
Iann Isacksson <iisackssonoliveira@gmail.com>
2020-09-06 10:08:04 -0300

    feat: Register console

--- src/app/services/CreateConsoleService.ts
+++ src/app/services/CreateConsoleService.ts
@@ -1,10 +1,16 @@
+import Console from '@app/schemas/Console';
+
 interface IRequest {
   name: string;
   company: string;
 }
 
 class CreateConsoleService {
-  public async execute({ name, company }: IRequest): Promise<void> {}
+  public async execute({ name, company }: IRequest): Promise<IRequest> {
+    const console = await Console.create({ name, company });
+
+    return console;
+  }
 }
 
 export default CreateConsoleService;
